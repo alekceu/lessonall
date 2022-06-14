f80bd5e19bfb22523bf9e4761540535224f4c020
Shved_Alekceu <shvedav86@gmail.com>
2022-06-14 21:07:47 +0300

    change scripts

--- lesson6/deploy.sh
+++ lesson6/deploy.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+#set rights on directory
+sudo chmod 777 /var/www/html/
+#create first page
+sudo cp /var/www/html/index.html /var/www/html/index1.html 
+sudo rm /var/www/html/index.html
+sudo echo "It is my first web page created automaticaly" > /var/www/html/index.html
+#restart apache
+sudo service apache2 restart
--- lesson6/install.sh
+++ lesson6/install.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+#install apache2
+sudo apt-get install  apache2
