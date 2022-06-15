b03b9a84d6d4adc04fecd345c25ad5f25bfc5418
Shved_Alekceu <shvedav86@gmail.com>
2022-06-15 20:01:52 +0300

    Repair scripts add -y flag

--- lesson6/startup.sh
+++ lesson6/startup.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+cd ~
+pwd
+whoami
+git clone https://github.com/alekceu/lessonall.git
+sudo chmod 555 lessonall/lesson6/install.sh
+./lessonall/lesson6/install.sh
+sudo chmod 555 lessonall/lesson6/deploy.sh
+./lessonall/lesson6/deploy.sh
